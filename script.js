16f08111b863167fc621024f6608a1df6601d000
Sara Marçal <143443978+saramarcal@users.noreply.github.com>
2023-12-06 12:56:57 -0300

    codigo termos de uso

---
+++ script.js
@@ -0,0 +1,29 @@
+fetch('arquivo.json')
+.then(response => response.json())
+.then(data => {
+  const informacoesProjeto = document.getElementById('informacoes-projeto');
+  const botaoToggle = document.getElementById('botao-toggle');
+
+  let isOpen = false;
+
+  botaoToggle.addEventListener('click', () => {
+    if (!isOpen) {
+      informacoesProjeto.innerHTML = `
+        <h2>Objetivo:</h2>
+        <p>${data.projeto.objetivo}</p>
+      `;
+      informacoesProjeto.style.display = 'block';
+      botaoToggle.innerText = 'Fechar objetivo';
+      isOpen = true;
+    } else {
+      informacoesProjeto.innerHTML = '';
+      informacoesProjeto.style.display = 'none';
+      botaoToggle.innerText = 'Conheça nosso objetivo';
+      isOpen = false;
+    }
+  });
+
+  // Definir texto inicial do botão
+  botaoToggle.innerText = 'Conheça nosso objetivo';
+})
+.catch(error => console.error('Erro ao carregar o JSON:', error));
